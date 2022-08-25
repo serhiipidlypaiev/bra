5f10b6db9f3d593c80ed994ff5ee5d19ea10d518
serhiipidlypaiev <sergey.podlipaev@gmail.com>
2022-08-25 13:01:36 +0300

    HW811

--- HW8/HW83_1.py
+++ HW8/HW83_1.py
@@ -1,7 +1,7 @@
 import time
 def animation(x = 10):
     for x in range(x):
-        symbols = (chr(92), chr(124), chr(47))
+        symbols = ('\\', '|', '/')
         for y in symbols:
             print(y*3, end="\r")
             time.sleep(0.5)
